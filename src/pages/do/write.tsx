77ece63a6f5c7cda459f3948968291539cb4ddcc
Hunter Heston <huntersamuelheston@gmail.com>
2023-08-12 08:49:20 -0700

    Added the basic UI elements needed for writing and publishing an article.

--- src/pages/do/write.tsx
+++ src/pages/do/write.tsx
@@ -10,20 +10,51 @@
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
+enum Visibility {
+  All = "All",
+  Approved = "Approved",
+  JustMe = "Just Me",
+  IntoTheEther = "Into the ether",
+}
+
 /**
  * This is the page where users write their posts.
  */
 export default function Write() {
+  const [title, setTitle] = useState("");
   const [text, setText] = useState("");
+  const [visibility, setVisibility] = useState<Visibility>(Visibility.All);
 
   return (
-    <div>
+    <div className="flex flex-col">
       <h1>Write your page article</h1>
+      <input
+        placeholder="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      ></input>
       <textarea
         value={text}
         onChange={(e) => setText(e.target.value)}
       ></textarea>
       <ReactMarkdown>{text}</ReactMarkdown>
+      <select onChange={(e) => setVisibility(e.target.value as Visibility)}>
+        <option value={Visibility.All}>{Visibility.All}</option>
+        <option value={Visibility.Approved}>{Visibility.Approved}</option>
+        <option value={Visibility.JustMe}>{Visibility.JustMe}</option>
+        <option value={Visibility.IntoTheEther}>
+          {Visibility.IntoTheEther}
+        </option>
+      </select>
+      <button onClick={() => onSend(title, text, visibility)}>Sent it</button>
     </div>
+  );
+}
+
+function onSend(title: string, text: string, visibility: string) {
+  console.log(
+    `title: ${title}
+text: ${text}
+visibility: ${visibility}`
   );
 }
