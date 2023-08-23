d40392bc968ba7ab9976913482199b479d5d104f
Hunter Heston <huntersamuelheston@gmail.com>
2023-08-23 15:59:06 -0700

    Remove

--- src/components/authentication/signInWithDiscord.tsx
+++
@@ -1,6 +0,0 @@
-import { signIn } from "next-auth/react";
-import { Button } from "../ui/button";
-
-export default function SignInWithDiscord() {
-  return <Button onClick={() => signIn()}>Sign in with Discord</Button>;
-}
